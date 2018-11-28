9529e91408d811ac95f09aa086a964012c10b207
Alexander Corcho <alexander.corcho@gmail.com>
2018-11-28 17:34:34 -0500

    carpeta con js para efectos de materia

---
+++ jsmaterial/script.js
@@ -0,0 +1,20 @@
+//boton flotante
+
+{
+document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.fixed-action-btn');
+        var instances = M.FloatingActionButton.init(elems,);
+      });
+
+}
+
+//sidebar
+{
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, );
+      });
+}
+
+
+
