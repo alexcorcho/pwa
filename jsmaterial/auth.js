3ff7b5425d643c535f5807b4c1d4c4a240464453
Alexander Corcho <alexander.corcho@gmail.com>
2018-11-30 14:46:43 -0500

    creacion de pagina config con sus respectivos cambios en el css

--- jsmaterial/auth.js
+++ jsmaterial/auth.js
@@ -8,15 +8,18 @@ var config = {
     messagingSenderId: "4711618046"
   };
   firebase.initializeApp(config);
+//finaliza firebase
 
+// funcion el boton de login
   function login(){     
-  var usuario = document.getElementById('usuario').value;
+    // capturra el dato de input de ususario, contraseña y lo gustda e una variable
+  var usuario = document.getElementById('usuario').value; 
   var pass = document.getElementById('pass').value; 
   
-
+    // sepasa por parametro el nombre de las varribles de arriba
     firebase.auth().signInWithEmailAndPassword(usuario, pass).catch(function(error) {
-        // Handle Errors here.c
-       
+        
+        // si los usuario no son correctos se muestra este mensaje
         alert("Usuario Invalido");
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -26,12 +29,15 @@ var config = {
   }
 
 
-
+  //funcion para crear nuevos usuarios
   function registro(){
+
+    // capturra el dato de input de ususario, contraseña y lo gustda e una variable
     var usuario2 = document.getElementById('usuario2').value;
     var pass2 = document.getElementById('pass2').value; 
+    // sepasa por parametro el nombre de las varribles de arriba
     firebase.auth().createUserWithEmailAndPassword(usuario2, pass2).catch(function(error) {
-      // Handle Errors here.
+      // erro si no se crea el usuario, puedes pasarlo en cosonle.log para saber que paso
       var errorCode = error.code;
       var errorMessage = error.message;
       // ...
@@ -40,11 +46,11 @@ var config = {
   }
   
 
-
+  //funcion para saber si ya inicio secion
   function observador(){
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          // usuario a ingresado.
+          // si ingreso enviarlo a este sitio
          
           window.open("dashboard.html","_self")
         } else {
@@ -54,5 +60,5 @@ var config = {
       });
   
   }
-
+// iniciar el observador 
   observador();
